988d8a25e93dbfd935e7b0594a7800f3ab51a39f
Daniel Gladson Arulraj P <danielgladson55@gmail.com>
2026-07-28 15:26:01 +0400

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/job-intelligence-dashboard/",
+
+  base: "/job-dashboard-repository/"
 });
